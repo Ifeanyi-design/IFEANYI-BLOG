5fd04c017ebe33df3e62f93da576c458b23dc960
Ifeanyi-design <ifeanyiagada123@gmail.com>
2025-08-01 15:28:51 +0100

    changed Bootstrap to bootstrap in post.html

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from functools import wraps
-
+import os
 from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
@@ -15,12 +15,12 @@ from urllib.parse import urlencode
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABSE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
